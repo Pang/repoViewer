bc6ffdb15544bcbb0de94efba4b7b81374f308f6
cPango <ryan.beckett@outlook.com>
2018-11-11 00:46:37 +0000

    removed the yellow autofill background, repos now appear on get request

--- src/app/repo-finder/repo-finder.component.ts
+++ src/app/repo-finder/repo-finder.component.ts
@@ -6,11 +6,13 @@ import { GithubApiService } from '../services/github-api.service'
   templateUrl: './repo-finder.component.html',
   styleUrls: ['./repo-finder.component.css']
 })
+
 export class RepoFinderComponent implements OnInit {
   constructor(private githubApi: GithubApiService) { }
 
   repositories: any[];
   gitProfile: string;
+  errorMsg: boolean;
 
   onSubmit(){
     this.githubApi.getRepos(this.gitProfile).subscribe(repo => this.repositories = repo.json());
