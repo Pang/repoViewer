a5bee4a4a9cf2e3dc94e8d1bb09c090648370f03
cPango <ryan.beckett@outlook.com>
2018-11-11 02:06:28 +0000

    created variables to be output to commit-finder

--- src/app/repo-finder/repo-finder.component.ts
+++ src/app/repo-finder/repo-finder.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output } from '@angular/core';
-import { GithubApiService } from '../services/github-api.service'
+import { GithubApiService } from '../services/github-api.service';
 
 @Component({
   selector: 'app-repo-finder',
@@ -13,12 +13,16 @@ export class RepoFinderComponent implements OnInit {
   repositories: any[];
   gitProfile: string;
   errorMsg: boolean;
+  @Output() repoCommits: any[];
+  @Output() selectedRepo: string;
 
   onSubmit(){
     this.githubApi.getRepos(this.gitProfile).subscribe(repo => this.repositories = repo.json());
   }
 
-  ngOnInit() {
+  selectRepo(){
+    this.githubApi.getCommits(this.gitProfile, this.selectedRepo).subscribe( commits => this.repoCommits = commits.json());
   }
 
+  ngOnInit(){}
 }
