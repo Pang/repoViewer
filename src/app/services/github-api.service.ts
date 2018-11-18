282dd6b14ba21c3321d6dd15298941eea95f1cb8
cPango <ryan.beckett@outlook.com>
2018-11-18 13:02:33 +0000

    registered app to github

--- src/app/services/github-api.service.ts
+++ src/app/services/github-api.service.ts
@@ -7,13 +7,15 @@ import { Http } from '@angular/http';
 
 export class GithubApiService {
   constructor(private _http:Http){}
+  private clientId:string = "Iv1.fe8de52d467bca23";
+  private clientSecret:string = "d05ba9baef1296718b19f241b4f533f2ab3d0890";
 
   getRepos(gitUser) {
-    return this._http.get(`https://api.github.com/users/${gitUser}/repos?per_page=100`);
+    return this._http.get(`https://api.github.com/users/${gitUser}/repos?client_id=${this.clientId}&client_secret=${this.clientSecret}`);
   }
 
   getCommits(gitUser, selectedRepo) {
-    return this._http.get(`https://api.github.com/repos/${gitUser}/${selectedRepo}/commits?per_page=100`);
+    return this._http.get(`https://api.github.com/repos/${gitUser}/${selectedRepo}/commits?client_id=${this.clientId}&client_secret=${this.clientSecret}`);
   }
   
 }
